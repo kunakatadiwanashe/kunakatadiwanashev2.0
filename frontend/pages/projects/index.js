1e04ef60fff53128f9a258982c2144b357bbe277
kunakatadiwanashe <kunakatadiwanashe99@gmail.com>
2025-03-11 09:16:25 +0200

    bloo

--- frontend/pages/projects/index.js
+++ frontend/pages/projects/index.js
@@ -1,14 +1,11 @@
-// import Head from "next/head";
+import Head from "next/head";
 
-// export default function projects() {
+export default function projects() {
 
     
-//     return <>
-//         <Head>
-//             <title>Project</title>
-//         </Head>
-//         <div className="projectpage">
-            
-//         </div>
-//     </>
-// }
+    return <>
+        <div className="">
+        <h1>Projects Page</h1>
+        </div>
+    </>
+}
