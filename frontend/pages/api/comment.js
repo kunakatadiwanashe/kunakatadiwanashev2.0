5cbb6f151e7447e44e26f80fa22bfde3faa51118
kunakatadiwanashe <kunakatadiwanashe99@gmail.com>
2025-03-15 14:54:40 +0200

    comments prt1 wrkng

--- frontend/pages/api/comment.js
+++ frontend/pages/api/comment.js
@@ -3,4 +3,39 @@ import { Comment } from "@/models/Comment";
 
 export default async function handle(req, res) {
     
+    await mongooseConnect();
+    const {method} = req;
+    if (method === 'POST') {
+        try {
+         const {name, email, title, contentpera, parent} = req.body;
+         let commentDoc;
+         if(parent) {
+                commentDoc = await Comment.create({
+                    name,
+                    email,
+                    title,
+                    contentpera,
+                    parent
+                });
+                await Comment.findByIdAndUpdate(parent, {
+                    $push: { children: commentDoc._id  }
+                });
+   
+         } else {
+            commentDoc = await Comment.create({
+                name,
+                email,
+                title,
+                contentpera
+            });
+         }
+         res.status(201).json(commentDoc);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({message: 'Internal server error'});
+        }
+    } else {
+        res.setHeader('Allow', ['POST']);
+        res.status(405).json({message: `Method ${method} not allowed`});
+    }
 }
--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -1,69 +1,208 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useState, useEffect } from 'react';
+import useFetchData from '@/hooks/useFetchData';
+import Link from 'next/link';
 
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-// import required modules
 import { FreeMode } from 'swiper/modules';
 import Head from 'next/head';
+import Spinner from '@/components/Spinner';
 
-export default function blogs() {
+export default function Blogs() {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [perPage] = useState(7);
+    const [searchQuery, setSearchQuery] = useState('');
+    const { alldata, loading } = useFetchData('/api/blogs');
 
+    useEffect(() => {
+        console.log('Fetched data:', alldata);
+    }, [alldata]);
 
-    return <>
-        <Head>
-            <title>Blogs</title>
-        </Head>
+    const paginate = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
 
+    const allblog = alldata.length;
 
-        <div className="blogpage">
-            <div className="tophero">
-                <div className="container">
+    const filteredBlogs = searchQuery.trim() === '' ? alldata : alldata.filter(blog => blog.title.toLowerCase().includes(searchQuery.toLowerCase()));
 
+    const indexOfFirstBlog = (currentPage - 1) * perPage;
+    const indexOfLastBlog = currentPage * perPage;
 
-                    <div className="toptitle">
-                        <div className="toptitlecont flex">
-                            <h1 className="">welcome to <span className="">KunTech Blogs</span></h1>
+    const currentBlogs = filteredBlogs.slice(indexOfFirstBlog, indexOfLastBlog);
 
-                            <div className="subemail">
-                                <form className="flex" action="" method="" >
-                                    <input
-                                        type="text"
-                                        id="id"
-                                        name="name"
-                                        placeholder="Search For A Blog"
-                                        className="w-[300px] border border-slate-200 rounded-lg py-3 px-5 outline-none	bg-transparent"
-                                    />
-                                    <button>Search</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
+    const publishedData = currentBlogs.filter(blog => blog.status === 'published');
+    const sliderpubdata = alldata.filter(blog => blog.status === 'published');
 
+    console.log('Slider data:', sliderpubdata);
+    console.log('Published data:', publishedData);
 
-                   <div className="featured">
+    const pageNumber = [];
+    for (let i = 1; i <= Math.ceil(allblog / perPage); i++) {
+        pageNumber.push(i);
+    }
+
+    return (
+        <>
+            <Head>
+                <title>Blogs</title>
+            </Head>
+
+            <div className="blogpage">
+                <div className="tophero">
                     <div className="container">
-                        <div className="featuredposts">
-                            <div className="postTitle flex">
-                                <h3 className="">Featured Post </h3>
+                        <div className="toptitle">
+                            <div className="toptitlecont flex">
+                                <h1>Welcome to <span>KunTech Blogs</span></h1>
+
+                                <div className="subemail">
+                                    <form className="flex" action="" method="">
+                                        <input
+                                            type="text"
+                                            id="id"
+                                            name="name"
+                                            placeholder="Search For A Blog"
+                                            className="w-[300px] border border-slate-200 rounded-lg py-3 px-5 outline-none bg-transparent"
+                                            value={searchQuery}
+                                            onChange={(e) => setSearchQuery(e.target.value)}
+                                        />
+                                        <button type="submit">Search</button>
+                                    </form>
+                                </div>
                             </div>
-                            <div className="featuredPosts flex">
-                                <Swiper>
-                                    
-                                </Swiper>
+                        </div>
+
+                        <div className="featured">
+                            <div className="container">
+                                <div className="featuredposts">
+                                    <div className="postTitle flex">
+                                        <h3>Featured Post</h3>
+                                    </div>
+                                    <div className="featuredPosts flex">
+                                        <Swiper
+                                            slidesPerView={'auto'}
+                                            freeMode={true}
+                                            spaceBetween={30}
+                                            modules={[FreeMode]}
+                                            className='mySwiper'
+                                        >
+                                            {loading ? <Spinner /> : (
+                                                sliderpubdata.slice(0, 6).map((blog) => (
+                                                    <SwiperSlide key={blog.id}>
+                                                        <div className="fpost">
+                                                            <Link href={`/blog/${blog.slug}`}>
+                                                                <img src={blog.images[0] || '/default-image.jpg'} alt={blog.title} />
+                                                            </Link>
+                                                            <div className="fpostinfo">
+                                                                <div className="tegs flex">
+                                                                    {Array.isArray(blog.blogcategory) ? (
+                                                                        blog.blogcategory.map((cat, index) => (
+                                                                            <Link key={index} href={`/blog/category${cat}`}>{cat}</Link>
+                                                                        ))
+                                                                    ) : (
+                                                                        <span>No categories available</span>
+                                                                    )}
+                                                                    <h2 className=""><Link href={`/blogs/${blog.slug}`}>{blog.title}</Link></h2>
+                                                                    <div className="flex">
+                                                                        <img src="" alt="" />
+                                                                        <p className="">kuntech</p>
+                                                                    </div>
+                                                                </div>
+
+                                                            </div>
+
+
+
+                                                        </div>
+                                                    </SwiperSlide>
+                                                ))
+                                            )}
+                                        </Swiper>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                   </div>
-
-
-
-
-
                 </div>
             </div>
 
-        </div>
-    </>
-}4
+
+
+
+            <section className="latestpostsec">
+                <div className="container">
+                    <div className="border"></div>
+                    <div className="latestpostsdat">
+                        <div className="title">
+                            <h3 className="">Latest Posts</h3>
+                        </div>
+
+                        <div className="latestposts">
+                            {loading ? <Spinner /> : <>
+                                {publishedData.map((blog) => {
+                                    return <div className='lpost' key={blog._id}>
+                                        <div className="lpostimg">
+                                            <Link href={`/blogs/${blog.slug}`}>
+                                                <img src={blog.images[0]} alt={blog.title} />
+                                            </Link>
+                                            <div className="tegs">
+                                                {Array.isArray(blog.blogcategory) ? (
+                                                    blog.blogcategory.map((cat, index) => (
+                                                        <Link key={index} href={`/blog/category${cat}`}>{cat}</Link>
+                                                    ))
+                                                ) : (
+                                                    <span>No categories available</span>
+                                                )}
+                                            </div>
+                                        </div>
+                                        <div className="info">
+                                                                    <h3><Link href={`/blogs/${blog.slug}`}>{blog.title}</Link></h3>
+                                                                    <p className="text-sm line-clamp-3">{blog.description} </p>
+                                                                </div>
+                                    </div>
+                                })}
+                            </>}
+                        </div>
+{/* 
+                        {publishedData.length === 0 ? (
+                               ""
+                            ) : (
+                                <div>
+                                    bu
+                                </div>
+                            )} */}
+
+
+
+
+                    </div>
+                </div>
+            </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </>
+    );
+}
